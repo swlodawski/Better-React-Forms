bb1f7e71ad4a67fb6b08e1e00e0e2492251ad7cc
sammy wlodawski <samwlodawski@gmail.com>
2024-12-10 08:51:41 -0500

    Finished form logic and am now setting up the jsx markup.

--- Better React Forms/src/App.jsx
+++ Better React Forms/src/App.jsx
@@ -17,8 +17,127 @@ function App() {
   const [selectedOption, setSelectedOption] = useState("");
   const [about, setAbout] = useState("");
 
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(
+        firstName,
+        lastName,
+        email,
+        contact,
+        gender,
+        selectedOption,
+        subjects,
+        resume,
+        url,
+        about
+      );
+    };
+
+    const handleSubjectChange = (sub) => {
+      setSubjects((prev) => ({
+        ...prev,
+        [sub]: !prev[sub],
+      }));
+    };
+
+    const handleReset = () => {
+      setFirstName('');
+      setLastName('');
+      setEmail('');
+      setContact('');
+      setGender('male');
+      setSelectedOption({
+        english: true,
+        maths: false,
+        physics: false
+      });
+      setSubjects('');
+      setResume('');
+      setUrl('');
+      setAbout('');
+    };
+
   return (
-    <div>Sup</div>
+    <div className='App'>
+      <h1>React Forms</h1>
+      <fieldset>
+        <form action="#" method='get'>
+          <label for="firstname">First Name</label>
+          <input 
+          type="text"
+          name='firstname'
+          id='firstname'
+          value={firstName}
+          onChange={(e) => 
+            setFirstName(e.target.value)
+          }
+          placeholder='Enter First Name' required />
+
+          <label for="lastname">Last Name</label>
+          <input 
+          type="text"
+          name='lastname'
+          id='lastname'
+          value={lastName}
+          onChange={(e) => 
+            setLastName(e.target.value)
+          }
+          placeholder='Enter Last Name' required />
+
+          <label for="email">Email</label>
+          <input 
+          type="text"
+          name='email'
+          id='email'
+          value={email}
+          onChange={(e) => 
+            setEmail(e.target.value)
+          }
+          placeholder='Enter Email' required />
+
+          <label for="tel">Contact</label>
+          <input 
+          type="tel"
+          name='contact'
+          id='contact'
+          value={contact}
+          onChange={(e) => 
+            setFirstName(e.target.value)
+          }
+          placeholder='Enter Mobile Number' required />
+
+          <label for="gender">Gender</label>
+          <input 
+          type="radio"
+          name='gender'
+          value='male'
+          checked={gender === "male"}
+          onChange={(e) => 
+            setGender(e.target.value)
+          } />
+          Male
+          <input 
+          type="radio"
+          name='gender'
+          value='female'
+          id='female'
+          checked={gender === "female"}
+          onChange={(e) => 
+            setGender(e.target.value)
+          } />
+          Female
+          <input
+          type="radio"
+          name='gender'
+          value='other'
+          id='other'
+          checked={gender === "other"}
+          onChange={(e) =>
+            setGender(e.target.value)
+          } />
+        </form>
+      </fieldset>
+    </div>
   );
 }
 
